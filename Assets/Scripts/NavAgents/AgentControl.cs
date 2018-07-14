83990f5f931e6b1880c2574966c2e33d718487c2
mikebru <michael.bruner3@gmail.com>
2018-07-14 18:15:50 -0500

    added in cat nav mesh agents

---
+++ Assets/Scripts/NavAgents/AgentControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentControl : MonoBehaviour {
+
+    public NavMeshAgent[] NavAgents;
+
+    [Range(.1f, 20)]
+    public float PathTimer;
+
+	// Use this for initialization
+	void Start () {
+
+        NavAgents = FindObjectsOfType<NavMeshAgent>();
+
+        StartCoroutine(MoveAgents());
+	}
+
+    IEnumerator MoveAgents()
+    {
+
+        for (int i = 0; i < NavAgents.Length; i++)
+        {
+            NavAgents[i].SetDestination(new Vector3(Random.Range(-7, 7), 0, Random.Range(-7, 7)));
+
+            yield return new WaitForSeconds(Random.Range(PathTimer, PathTimer+.2f));
+
+        }
+
+        StartCoroutine(MoveAgents());
+
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/NavAgents/CatControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CatControl : MonoBehaviour {
+
+    Animator anim;
+    NavMeshAgent agent;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        anim.SetFloat("Speed", agent.velocity.magnitude/agent.speed);
+
+	}
+}
